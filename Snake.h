0f243780db7543450ef397ff1b1b49fdc347f39e
qewrty12 <105336858+qewrty12@users.noreply.github.com>
2023-06-20 00:46:08 +0900

    Update Snake.h

--- Snake.h
+++ Snake.h
@@ -2,13 +2,9 @@
 #include "Snake_map.h"
 #include "Point.h"
 #include "Item.h"
-
 #ifndef __SNAKE__
 #define __SNAKE__
-
-//snake의 방향
-enum direction { WEST, NORTH, EAST, SOUTH }; //서: 0 북:1 동:2 남:3
-
+enum direction { WEST, NORTH, EAST, SOUTH }; 
 class Snake {
 public:
 // private:
@@ -22,7 +18,6 @@ public:
     bool turnDirection(int key);
     void setSyncTime(float value);
     void movebyDoor();
-    //move 함수를 통해 item과 door만났을 때 모두 처리
     bool move(Point *p, Item* item);
 };
 
