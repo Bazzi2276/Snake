cd0bca45b9202d3c399f34b1e538e514f981a4b6
qewrty12 <105336858+qewrty12@users.noreply.github.com>
2023-06-20 00:38:59 +0900

    Update Display.h

--- Display.h
+++ Display.h
@@ -3,13 +3,14 @@
 #include "Point.h"
 #include "Mission.h"
 
-void draw_snakewindow(WINDOW* snake_win);
+void displayNext(WINDOW* notice_win);
+void displayGameClear(WINDOW* notice_win);
+
 void colorSetting();
 
 void updatePoint(WINDOW* point_win, Point* p);
 void updateMission(WINDOW* mission_win, Mission* m, Point* p);
+void draw_snakewindow(WINDOW* snake_win);
 
-//stage notice
-void displayNext(WINDOW* notice_win);
-void displayGameClear(WINDOW* notice_win);
+
 void displayDead(WINDOW* notice_win);
