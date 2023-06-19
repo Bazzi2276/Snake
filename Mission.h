742fa5d0ffacd89f2b9d217a97c971b0f49e8196
qewrty12 <105336858+qewrty12@users.noreply.github.com>
2023-06-20 00:43:44 +0900

    Update Mission.h

--- Mission.h
+++ Mission.h
@@ -3,14 +3,14 @@
 
 class Mission {
 private:
-    int B;
-    int growItem;
-    int poisonItem;
-    int useGate;
     bool Bflag;
     bool growItemFlag;
     bool poisonItemFlag;
     bool useGateFlag;
+    int B;
+    int growItem;
+    int poisonItem;
+    int useGate;
 public:
     Mission();
     int getB();
@@ -18,25 +18,24 @@ public:
     int getPoisonItem();
     int getUseGate();
 
-    //Mission 값 설정 
-    void setB(int b);
-    void setGrowItem(int x);
-    void setPoisonItem(int x);
-    void setUseGate(int x);
 
-    //현재 Point와 비교해서 Mission 달성 여부 확인
     void checkB(Point *p);
     void checkGrowItem(Point *p);
     void checkPoisonItem(Point *p);
     void checkUseGate(Point *p);
     void updateAll(Point *p);
+    void setB(int b);
+    void setGrowItem(int x);
+    void setPoisonItem(int x);
+    void setUseGate(int x);
 
-    //각각의 목표를 clear했는지 
+
+
     bool getBflag();
     bool getGrowItemFlag();
     bool getPoisonItemFlag();
     bool getUseGateFlag();
 
-    //모든 목표를 clear했는지
+
     bool checkStageClear();
 };
