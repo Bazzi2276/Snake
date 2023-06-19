178301c918aebf44304f19b9a8db321f2771d944
qewrty12 <105336858+qewrty12@users.noreply.github.com>
2023-06-20 00:47:09 +0900

    Update Snake_map.h

--- Snake_map.h
+++ Snake_map.h
@@ -3,14 +3,6 @@
 #define __SNAKE_MAP__
 #define MAP_SIZE 21
 #define GATE_SIZE 2
-        //     0        1         2        3           4           5            6         7
-enum color {WHITE, IMMUNE_WALL, WALL, SNAKE_HEAD, SNAKE_BODY, GROWTH_ITEM, POISON_ITEM, GATE,X_ITEM};
-
-extern int stage;
-extern bool waitGate;
-extern int forGate;
-extern bool waitRandomWall;
-extern int forRandomWall;
 
 extern int gate[][GATE_SIZE];
 extern int map[MAP_SIZE][MAP_SIZE];
@@ -19,6 +11,15 @@ extern int map2[][GATE_SIZE];
 extern int map3[][GATE_SIZE];
 extern int map4[][GATE_SIZE];
 extern int walls[][GATE_SIZE];
+extern bool waitRandomWall;
+extern int forRandomWall;
+extern int stage;
+extern bool waitGate;
+extern int forGate;
+
+        //     0        1         2        3           4           5            6         7
+enum color {WHITE, IMMUNE_WALL, WALL, SNAKE_HEAD, SNAKE_BODY, GROWTH_ITEM, POISON_ITEM, GATE,X_ITEM};
+
 
 void map_init(int stage);
 void findImmuneWall();
